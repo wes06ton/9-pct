2957c8d1dc56025414d4d405be828968d3a6764c
wes06ton <westonkpaschall@gmail.com>
2025-03-06 10:49:36 -0600

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {
+ type = string
+}
+variable "client_id" {
+ type = string
+}
+variable "client_secret" {
+ type = string
+}
+variable "tenant_id" {
+ type = string
+}
+variable "ssh_public_key" {
+ description = "SSH public key for VM access"
+ type = string
+}
